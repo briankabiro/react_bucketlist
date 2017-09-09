ebdc5b453206afdd726812b9f07b67463dcbb687
brian kabiro <briankigondu@gmail.com>
2017-09-09 23:57:23 +0300

    Add Links to Home Container
    I moved the links from the Router component to the home component

--- src/containers/router.js
+++ src/containers/router.js
@@ -2,8 +2,7 @@ import React from 'react';
 
 import {
 	BrowserRouter as Router,
-	Route,
-	Link
+	Route
 } from 'react-router-dom';
 
 import Home from './home'
@@ -15,12 +14,6 @@ import PrivateRoute from './private';
 export default () => (
 	<Router>
 		<div>
-			<ul>
-				<li><Link to="/">Bucketlist</Link></li>
-				<li><Link to="/login">Login</Link></li>
-				<li><Link to="/register">Register</Link></li>
-			</ul>
-
 			<Route exact path="/" component={Home}/>
 			<Route path="/register" component={Register}/>
 			<Route path="/login" component={Login}/>
