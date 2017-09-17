e68c521b75b03b9538ea88fcaba033e3447ea515
brian kabiro <briankigondu@gmail.com>
2017-09-17 13:49:37 +0300

    Add Styles to Dashboard container

--- src/components/add_bucketlist.js
+++ src/components/add_bucketlist.js
@@ -1,22 +1,11 @@
 import React from 'react'
-import {Col, Form, FormGroup, ControlLabel, FormControl, Button} from 'react-bootstrap'
+import {Form} from 'react-bootstrap'
+import '../styles/dashboard.css'
 
 export default (props) => (
-	<Form onSubmit={(event) => props.handleSubmit(event)}>
-		<FormGroup controlId="formHorizontalUsername">
-			<Col sm={5} smPush={3}>
-		    	<FormControl type="text" name = "name" placeholder="Add name of bucketlist" required/>
-				<Button type="submit">Create</Button>
-			</Col>
-		</FormGroup>
-		
-	    <FormGroup>
-	    	<Col smPush={5}>
-		        <Button type="submit">
-		          Create
-		    	</Button>
-	    	</Col>
-	    </FormGroup>
+	<Form className="add-bucketlist-form" onSubmit={(event) => props.handleSubmit(event)}>
+    	<input type="text" name = "name" placeholder="Add name of bucketlist" required/>
+		<button type="submit">Create</button>
 	</Form>
 
 )
