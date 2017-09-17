b413643752763d381d27a55fbc837aec45e2760a
brian kabiro <briankigondu@gmail.com>
2017-09-17 09:49:44 +0300

    Add Register Form
    I created the register form component and moved the form from the register container

---
+++ src/components/register_form.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import {FormControl, FormGroup, Col, ControlLabel, Form, Button} from 'react-bootstrap'
+
+export default (props) => (
+	<Form horizontal onSubmit={props.handleSubmit}>
+
+		<FormGroup controlId="formHorizontalUsername">
+			<Col componentClass={ControlLabel} sm={2}>
+			 	Username
+			</Col>
+		  	<Col sm={10}>
+		    	<FormControl type="text" name = "username" placeholder="Username" required/>
+			</Col>
+		</FormGroup>
+		
+		<FormGroup controlId="formHorizontalPassword">
+			<Col componentClass={ControlLabel} sm={2}>
+		    	Password
+		  	</Col>
+			<Col sm={10}>
+				<FormControl type="password" name="password" placeholder="Password" required/>
+			</Col>
+		</FormGroup>
+		
+	    <FormGroup>
+	    	<Col smOffset={2} sm={10}>
+		        <Button type="submit">
+		          Register
+		    	</Button>
+	    	</Col>
+	    </FormGroup>
+	</Form>
+)
