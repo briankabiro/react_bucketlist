5dd045141ff224b42529e0cd9a8cac91a81ae923
brian kabiro <briankigondu@gmail.com>
2017-09-17 13:50:11 +0300

    Update Login Form Component

--- src/components/login_form.js
+++ src/components/login_form.js
@@ -1,11 +1,33 @@
 import React from 'react'
+import {FormControl, FormGroup, Col, ControlLabel, Form, Button} from 'react-bootstrap'
 
 export default (props) => (
-	<form className= "form" onSubmit={props.handleSubmit}>
-		<label>Username</label>
-		<input type="text" name = "username" placeholder="Username"/>
-		<label>Password</label>
-		<input type="password" name = "password" placeholder="Password"/>
-		<button type='submit'>Login</button>
-	</form>
+	<Form horizontal onSubmit={props.handleSubmit}>
+
+		<FormGroup controlId="formHorizontalUsername">
+			<Col componentClass={ControlLabel} sm={2}>
+				Username
+			</Col>
+			<Col sm={10}>
+				<FormControl type="text" name = "username" placeholder="Username" required/>
+			</Col>
+		</FormGroup>
+		
+		<FormGroup controlId="formHorizontalPassword">
+			<Col componentClass={ControlLabel} sm={2}>
+				Password
+			</Col>
+			<Col sm={10}>
+				<FormControl type="password" name="password" placeholder="Password" required/>
+			</Col>
+		</FormGroup>
+		
+		<FormGroup>
+			<Col smOffset={2} sm={10}>
+				<Button type="submit">
+				  Login
+				</Button>
+			</Col>
+		</FormGroup>
+	</Form>
 )
