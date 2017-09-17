3908c6fb296a85a5965d682b0264ee43a15ff954
brian kabiro <briankigondu@gmail.com>
2017-09-17 09:53:22 +0300

    Update Router
    I updated the Router component by including all the routes in a Switch tag

--- src/containers/router.js
+++ src/containers/router.js
@@ -2,23 +2,28 @@ import React from 'react';
 
 import {
 	BrowserRouter as Router,
-	Route
+	Route,
+	Switch
 } from 'react-router-dom';
 
 import Home from './home'
 import Register from './register_container';
 import Login from './login_container';
 import Dashboard from './dashboard';
+import Item_Dashboard from './item_dashboard'
 import PrivateRoute from './private';
+import NoMatch from '../components/no_match';
 
 export default () => (
 	<Router>
-		<div>
+		<Switch>
 			<Route exact path="/" component={Home}/>
 			<Route path="/register" component={Register}/>
 			<Route path="/login" component={Login}/>
-			<PrivateRoute path="/dashboard" component={Dashboard}/>
-		</div>
+			<PrivateRoute path="/bucketlists/:id" component={Item_Dashboard} />
+			<PrivateRoute path="/bucketlists" component={Dashboard}/>
+			<Route component={NoMatch} />
+		</Switch>
 	</Router>
 )
 
