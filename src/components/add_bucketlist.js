1de32c209711dff6c71e438c4747de4eb54693ae
brian kabiro <briankigondu@gmail.com>
2017-09-21 17:59:00 +0300

    Update add_bucketlist component styling

--- src/components/add_bucketlist.js
+++ src/components/add_bucketlist.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import {Form, Button} from 'reactstrap'
+import {Form, Button, Input, Col} from 'reactstrap'
 import '../styles/dashboard.css'
 
 const AddBucket = props => (
-	<Form className="add-bucketlist-form" onSubmit={(event) => props.handleSubmit(event)}>
-    <input id="input" type="text" name = "name" placeholder="Add name of bucketlist" required />
-		<input type="text" name = "name" placeholder="Add name of bucketlist" required />
-		<Button color="primary "type="submit">Create</Button>
-	</Form>
+	<Col md={{ size: 4, offset: 4 }}>
+		<Form className="add-bucketlist-form" onSubmit={(event) => props.handleSubmit(event)}>
+			<Input type="text" name = "name" placeholder="Add name of bucketlist" required />
+			<Button color="primary "type="submit">Create</Button>
+		</Form>
+	</Col>
 );
 
 export default AddBucket;
