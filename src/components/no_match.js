30b3988d03714151399f1566847b6486a96d3a32
brian kabiro <briankigondu@gmail.com>
2017-09-26 14:18:07 +0300

    Update styling of no-match component

--- src/components/no_match.js
+++ src/components/no_match.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Jumbotron, Button } from 'reactstrap';
+import { Button } from 'reactstrap';
 
 export default () => (
-    <Jumbotron>
+    <div className="container d-flex-column justify-content-center">
         <h1>Oops!</h1>
         <h2>404 Not Found</h2>
         <p>
             Sorry, an error has occured, Requested page not found!
         </p>
+
         <div>
             <Link to="/">
 	            <Button className="btn-primary">
@@ -16,5 +17,5 @@ export default () => (
 	            </Button>
             </Link>
         </div>
-    </Jumbotron>
+    </div>
 )
