d2e4a6bc4f49275f2db5d43a1b888693059daf84
brian kabiro <briankigondu@gmail.com>
2017-09-28 17:30:23 +0300

    Update Icons
    I updated the button for adding a bucketlist and an item to be a plus button

--- src/components/add_bucketlist.js
+++ src/components/add_bucketlist.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import {Form, Button, Input, Col} from 'reactstrap'
+import {Form, Button, Input, Col} from 'reactstrap';
+import { FaPlus } from 'react-icons/lib/fa';
 import '../styles/dashboard.css'
 
 const AddBucket = props => (
 	<Col md={{ size: 4, offset: 4 }}>
 		<Form className="add-bucketlist-form" onSubmit={(event) => props.handleSubmit(event)}>
 			<Input type="text" name = "name" placeholder="Add name of bucketlist" required />
-			<Button color="primary "type="submit">Create</Button>
+			<Button color="primary "type="submit"><FaPlus /></Button>
 		</Form>
 	</Col>
 );
--- src/components/add_item.js
+++ src/components/add_item.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Form, Input, Col, Button} from 'reactstrap';
+import { FaPlus } from 'react-icons/lib/fa';
 import '../styles/item_dashboard.css';
 
 export default (props) => (
 	<Col md={{ size: 4, offset: 4 }}>
 		<Form className="add-item-form" onSubmit={props.handleSubmit}>
 			<Input type="text" name="item_name" placeholder="Name of the item" required/>
-			<Button color="primary" type="submit">Add Item</Button>
+			<Button color="primary" type="submit"><FaPlus /></Button>
 		</Form>
 	</Col>
 )
