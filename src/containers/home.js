22936e101abb677ca0872d2002422252235d77e0
brian kabiro <briankigondu@gmail.com>
2017-10-03 19:23:55 +0300

    [Feature] Update homepage
    I updated the homepage of the site by adding styles and div sections

--- src/containers/home.js
+++ src/containers/home.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from 'reactstrap';
+import '../styles/home.css';
 
 export default () => (
-	<div>
-		<Link to="/login">Login</Link>
-		<Link to="/register">Register</Link>
-		<h1>Bucketlist</h1>
+	<div className="body">
+		<div className="nav-bar">
+			<h1>Bucketlist</h1>
+			<div className="right-nav-links">
+				<Link to="/login">Login</Link>
+				<Link to="/register">Register</Link>
+			</div>
+		</div>
+
+		<div className="content">
+			<p>Create the bucketlist of your dreams</p>
+			<Link to="/register"><Button color="primary">Register</Button></Link>
+		</div>
 	</div>
 )
